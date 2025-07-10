2113c0f0afa4b6d3f5386f0d1f38131aeea0615a
Quan Tran <quan.tran@zoi.tech>
2025-07-10 12:28:44 +0700

    fix: add notificationClientRegistry

--- index.js
+++ index.js
@@ -1,95 +1,175 @@
 const fs = require("fs");
-const { load } = require("ion-js");
 const { parse } = require("json2csv");
-const { ungzip, gzip } = require("node-gzip");
+const zlib = require("zlib");
+const readline = require("node:readline");
+const { unmarshall } = require("@aws-sdk/util-dynamodb");
 
-function processIonData(data, transformFlag) {
-  const items = [];
-  let currentItem = {};
-  data.forEach((item) => {
-    if (item.Item) {
-      if (Object.keys(currentItem).length !== 0) {
-        items.push(currentItem);
-        currentItem = {};
-      }
-      currentItem = item.Item;
-    }
-  });
+const OUTPUT_FILENAME = "output_combined.csv";
+const FIELDS = ["PK", "SK", "country", "userId"];
 
-  if (transformFlag) {
-    return items
-      .map((item) => ({
-        PK: `COUNTRY#${item.country}`,
-        SK: `#USER#${item.userId}`,
-        country: item.country,
-        userId: item.userId,
-      }))
-      .filter((item) => item.country == "VN"); // filter for country
-  } else {
-    return items.map((item) => ({
-      PK: item.PK,
-      SK: item.SK,
-      country: item.country,
-      userId: item.userId,
-    }));
-  }
+const DATA_TYPE = {
+  NOTIFICATION: "notification",
+  PROFILE: "profile",
+  USER_SEGMENT: "userSegment",
+};
+
+function handleNotification(data) {
+  if (data.language != "vi") return null;
+  return {
+    PK: `COUNTRY#CZ`,
+    SK: `#USER#${data.userId}`,
+    country: `CZ`,
+    userId: data.userId,
+  };
 }
 
-async function processIonGzFiles(fileList, outputFileName, transformFlag) {
-  const combinedData = [];
+function handleUserSegment(data) {
+  return {
+    PK: data.PK,
+    SK: data.SK,
+    country: data.country,
+    userId: data.userId,
+  };
+}
 
+function handleUserProfile(data) {
+  if (data.country != "VN") return null;
+  return {
+    PK: `COUNTRY#${data.country}`,
+    SK: `#USER#${data.userId}`,
+    country: data.country,
+    userId: data.userId,
+  };
+}
+
+const handlers = {
+  [DATA_TYPE.NOTIFICATION]: handleNotification,
+  [DATA_TYPE.PROFILE]: handleUserProfile,
+  [DATA_TYPE.USER_SEGMENT]: handleUserSegment,
+};
+
+function processDynamoDBData(line, transformFlag) {
+  if (!line) return;
+  const parsed = JSON.parse(line);
+  const data = unmarshall(parsed.Item);
+  return handlers[transformFlag]?.(data);
+}
+
+const statistics = {};
+
+async function processDynamoDBJsonFiles(
+  fileList,
+  OUTPUT_FILENAME,
+  transformFlag
+) {
   for (const file of fileList) {
-    console.log(`Processing file: ${file}`);
+    console.log(`Processing file: \x1b[36m${file}\x1b[0m`);
     if (!fs.existsSync(file)) {
-      console.error(`File not found: ${file}`);
+      console.error(`\x1b[31mFile not found: ${file}\x1b[0m`);
       continue;
     }
-    const data = await ungzip(fs.readFileSync(file, null));
-    const decodedData = data.toString("utf8");
-    const lines = decodedData.split("\n").filter((line) => line.trim() !== "");
 
-    const ionData = lines.map(load);
+    const readStream = fs.createReadStream(file);
+    const unzipStream = readStream.pipe(zlib.createGunzip());
 
-    const processData = processIonData(ionData, transformFlag);
-    if (processData.length > 0) {
-      combinedData.push(...processIonData(ionData, transformFlag));
-    }
+    const rl = readline.createInterface({
+      input: unzipStream,
+      crlfDelay: Infinity,
+    });
+
+    const outputStream = fs.createWriteStream(OUTPUT_FILENAME, { flags: "a" });
+
+    let lineCount = 0;
+    rl.on("line", (line) => {
+      lineCount++;
+      process.stdout.write(
+        `\r\x1b[32m${file}\x1b[0m - line \x1b[33m${lineCount}\x1b[0m`
+      );
+      const data = processDynamoDBData(line, transformFlag);
+      if (!data) return;
+      statistics[data.country] = (statistics[data.country] || 0) + 1;
+      const csv = parse([data], {
+        fields: ["PK", "SK", "country", "userId"],
+        header: false,
+      });
+      outputStream.write(csv + "\n");
+    });
+
+    await new Promise((resolve) => {
+      rl.on("close", () => {
+        process.stdout.write("\n");
+        outputStream.end();
+        resolve();
+      });
+    });
   }
-
-  const isOutputFileExist = fs.existsSync(outputFileName);
-
-  const csv = parse(combinedData, {
-    fields: ["PK", "SK", "country", "userId"],
-    header: !isOutputFileExist,
-  });
-
-  fs.appendFileSync(outputFileName, isOutputFileExist ? "\n" + csv : csv);
 }
 
 async function start() {
-  const outputFileName = "output_combined.csv";
-  console.log("Processing UserSegments .ion.gz files...");
+  if (fs.existsSync(OUTPUT_FILENAME)) {
+    console.log(
+      `\x1b[31m${OUTPUT_FILENAME}\x1b[0m does exist. Delete it before start.`
+    );
+    return;
+  }
+
+  fs.writeFileSync(
+    OUTPUT_FILENAME,
+    parse([], { fields: FIELDS, header: true }) + "\n"
+  );
+
+  console.log("Processing Notification .json.gz files...");
+  const userNotification = [
+    "./data/notifications/3gwsd3mpzm4g3o577pkqajynzm.json.gz",
+    "./data/notifications/3mplb62geq4hrkxdygbpgws3ra.json.gz",
+    "./data/notifications/6dph5ocfbmzxpbcj3oah6hvrxu.json.gz",
+    "./data/notifications/y3gmlzyqvq5xtpmoeei3jhraka.json.gz",
+  ];
+  await processDynamoDBJsonFiles(
+    userNotification,
+    OUTPUT_FILENAME,
+    DATA_TYPE.NOTIFICATION
+  );
+
+  console.log("Processing UserSegment .json.gz files...");
   const userSegments = [
-    "./data/user_segments/44dazuseku5e5pc4chdr22iqy4.ion.gz",
+    "./data/user_segments/p5hvavbzdu2iphoqyezmh53fri.json.gz",
   ];
-  await processIonGzFiles(userSegments, outputFileName, false);
+  await processDynamoDBJsonFiles(
+    userSegments,
+    OUTPUT_FILENAME,
+    DATA_TYPE.USER_SEGMENT
+  );
 
-  console.log("Processing UserProfiles .ion.gz files...");
+  console.log("Processing UserProfile .json.gz files...");
   const userProfiles = [
-    "./data/user_profiles/i6cqcxv7y455njoqgzpws47sf4.ion.gz",
-    "./data/user_profiles/ooalwe6jly7hvmbdkaaqkopmbu.ion.gz",
-    "./data/user_profiles/pmhqknwzxa2ljnypmtbckxsr5e.ion.gz",
-    "./data/user_profiles/zv4bixxjry2hppgopemle7saoy.ion.gz",
+    "./data/user_profiles/3ubo7oa3dq26tdriq34polqili.json.gz",
+    "./data/user_profiles/cfpo6voetayxpljgrctpuv4ivi.json.gz",
+    "./data/user_profiles/l76iacmd6a5ujdmm2d4dtqiypa.json.gz",
+    "./data/user_profiles/t7x6tatmre6wllbubtdhrqup6q.json.gz",
   ];
-  await processIonGzFiles(userProfiles, outputFileName, true);
-
-  console.log("Data processed and combined into a single CSV file");
-
-  const gzippedData = await gzip(fs.readFileSync(outputFileName));
-  fs.writeFileSync(outputFileName + ".gzip", gzippedData);
+  await processDynamoDBJsonFiles(
+    userProfiles,
+    OUTPUT_FILENAME,
+    DATA_TYPE.PROFILE
+  );
 
   console.log(
-    `Output written to ${outputFileName} and gzipped to ${outputFileName}.gz`
+    "\x1b[42m\x1b[30mFINISH:\x1b[0m Data processed and combined into a single CSV file"
+  );
+
+  console.log(`Statistics by country:`);
+  Object.keys(statistics).forEach((country) => {
+    console.log(
+      `\x1b[32mCountry: ${country}\x1b[0m, Count: \x1b[33m${statistics[country]}\x1b[0m`
+    );
+  });
+
+  const gzippedData = await gzip(fs.readFileSync(OUTPUT_FILENAME));
+  fs.writeFileSync(OUTPUT_FILENAME + ".gzip", gzippedData);
+
+  console.log(
+    `Output written to ${OUTPUT_FILENAME} and gzipped to ${OUTPUT_FILENAME}.gz`
   );
 }
 
